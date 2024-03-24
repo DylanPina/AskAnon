f91ac29fb80bbb47c0c82296d752d00193d8e0fe
Dylan Pina <77121344+DylanPina@users.noreply.github.com>
2024-03-24 09:51:00 -0400

    Sorted messages by time stamp

--- components/ui/session-layout/message-list.tsx
+++ components/ui/session-layout/message-list.tsx
@@ -12,22 +12,37 @@ interface Props {
 export default function MessageList({ sessionId }: Props) {
   const { messages, loading } = useSessionMessages(sessionId);
   console.log(`Messages: ${JSON.stringify(messages)}`);
-  const groupedMessages = messages.reduce((acc: any, message: any) => {
-    const lastGroup = acc[acc.length - 1];
-    if (lastGroup && lastGroup[lastGroup.length - 1].uid === message.uid) {
-      lastGroup.push(message);
-    } else {
-      acc.push([message]);
-    }
-    return acc;
-  }, [] as Message[][]);
+
+  const sortedMessages = messages.sort((a: Message, b: Message) => {
+    const aTime = a.createdAt.seconds + a.createdAt.nanoseconds * 1e-9;
+    const bTime = b.createdAt.seconds + b.createdAt.nanoseconds * 1e-9;
+    return aTime - bTime;
+  });
+
+  const groupedMessages = sortedMessages.reduce(
+    (acc: Message[][], message: Message) => {
+      const lastGroup = acc[acc.length - 1];
+      if (lastGroup && lastGroup[lastGroup.length - 1].uid === message.uid) {
+        lastGroup.push(message);
+      } else {
+        acc.push([message]);
+      }
+      return acc;
+    },
+    [],
+  );
 
   return (
     <div className="flex flex-col place-self-center space-y-4 h-full max-w-[1200px] overflow-y-auto w-full pt-4">
-      {groupedMessages.length === 0 ? (
-        <div className="text-center py-4 text-white font-bold text-2xl">No messages... yet</div>
+      {loading && <div>Loading...</div>}
+      {!loading && groupedMessages.length === 0 ? (
+        <div className="text-center py-4 text-white font-bold text-2xl">
+          No messages... yet
+        </div>
       ) : (
-        groupedMessages.map((group: any, index: any) => <MessageBubble key={index} group={group} />)
+        groupedMessages.map((group: Message[], index: number) => (
+          <MessageBubble key={index} group={group} />
+        ))
       )}
     </div>
   );
