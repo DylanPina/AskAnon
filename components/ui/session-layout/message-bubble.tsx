35882038699edf10578f8272cdda2cb7b0b86e86
Jack Bogart <jackbogart0822@gmail.com>
2024-03-23 20:21:17 -0400

    Add a scrollbar when number of messages overflows
    Before this the site's height would continue to expand infinitely, this
    is a clean alternative to navigate messages.

--- components/ui/session-layout/message-bubble.tsx
+++ components/ui/session-layout/message-bubble.tsx
@@ -1,31 +1,35 @@
-import React from "react";
-import { Message } from "@/lib/definitions/message";
+import React from 'react';
+import { Message } from '@/lib/definitions/message';
 
 type MessageBubbleProps = {
-  group: Message[];
+    group: Message[];
 };
 
 export const MessageBubble: React.FC<MessageBubbleProps> = ({ group }) => (
-  <div className="flex items-start ml-4">
-    {group.length > 0 && (
-      <div className="w-10 h-10 rounded-full bg-gray-200 flex items-center justify-center mt-6">
-        {group[0].fakeName.slice(0, 2).toUpperCase()}
-      </div>
-    )}
-    <div className="flex flex-col space-y-2 text-s max-w-xs mx-2 items-start">
-      {group.map((message, idx) => (
-        <div key={idx}>
-          {idx === 0 && (
-            <>
-              <span className="text-white">{message.fakeName}</span>
-              <span className="ml-2 text-sm text-gray-500">{new Date(message.createdAt).toLocaleTimeString()}</span>
-            </>
-          )}
-          <div>
-            <span className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">{message.content}</span>
-          </div>
+    <div className="flex items-start ml-4">
+        {group.length > 0 && (
+            <div className="w-10 h-10 rounded-full bg-gray-200 flex items-center justify-center mt-6">
+                {group[0].fakeName.slice(0, 2).toUpperCase()}
+            </div>
+        )}
+        <div className="flex flex-col space-y-2 text-s max-w-xs mx-2 items-start">
+            {group.map((message, idx) => (
+                <div key={idx}>
+                    {idx === 0 && (
+                        <>
+                            <span className="text-white">{message.fakeName}</span>
+                            <span className="ml-2 text-sm text-gray-500">
+                                {new Date(message.createdAt).toLocaleTimeString()}
+                            </span>
+                        </>
+                    )}
+                    <div>
+                        <span className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">
+                            {message.content}
+                        </span>
+                    </div>
+                </div>
+            ))}
         </div>
-      ))}
     </div>
-  </div>
 );
--- lib/mock-data/messages-mock-data.ts
+++ lib/mock-data/messages-mock-data.ts
@@ -1,40 +1,112 @@
-import { Message } from "../definitions/message";
+import { Message } from '../definitions/message';
 
 export const mockMessages: Message[] = [
-  {
-    uid: "1",
-    fakeName: "Anonymous Moose",
-    content: "content",
-    createdAt: new Date(),
-  },
-  {
-    uid: "1",
-    fakeName: "Anonymous Moose",
-    content: "more content",
-    createdAt: new Date(),
-  },
-  {
-    uid: "1",
-    fakeName: "Anonymous Moose",
-    content: "even more content",
-    createdAt: new Date(),
-  },
-  {
-    uid: "2",
-    fakeName: "Angry Panda",
-    content: "Can you go over problem 3?",
-    createdAt: new Date(),
-  },
-  {
-    uid: "2",
-    fakeName: "Angry Panda",
-    content: "Can you go over problem 4?",
-    createdAt: new Date(),
-  },
-  {
-    uid: "3",
-    fakeName: "Elongated Muskrat",
-    content: "What class is this?",
-    createdAt: new Date(),
-  },
+    {
+        uid: '1',
+        fakeName: 'Anonymous Moose',
+        content: 'content',
+        createdAt: new Date(),
+    },
+    {
+        uid: '1',
+        fakeName: 'Anonymous Moose',
+        content: 'more content',
+        createdAt: new Date(),
+    },
+    {
+        uid: '1',
+        fakeName: 'Anonymous Moose',
+        content: 'even more content',
+        createdAt: new Date(),
+    },
+    {
+        uid: '2',
+        fakeName: 'Angry Panda',
+        content: 'Can you go over problem 3?',
+        createdAt: new Date(),
+    },
+    {
+        uid: '2',
+        fakeName: 'Angry Panda',
+        content: 'Can you go over problem 4?',
+        createdAt: new Date(),
+    },
+    {
+        uid: '3',
+        fakeName: 'Elongated Muskrat',
+        content: 'What class is this?',
+        createdAt: new Date(),
+    },
+    {
+        uid: '1',
+        fakeName: 'Anonymous Moose',
+        content: 'content',
+        createdAt: new Date(),
+    },
+    {
+        uid: '1',
+        fakeName: 'Anonymous Moose',
+        content: 'more content',
+        createdAt: new Date(),
+    },
+    {
+        uid: '1',
+        fakeName: 'Anonymous Moose',
+        content: 'even more content',
+        createdAt: new Date(),
+    },
+    {
+        uid: '2',
+        fakeName: 'Angry Panda',
+        content: 'Can you go over problem 3?',
+        createdAt: new Date(),
+    },
+    {
+        uid: '2',
+        fakeName: 'Angry Panda',
+        content: 'Can you go over problem 4?',
+        createdAt: new Date(),
+    },
+    {
+        uid: '3',
+        fakeName: 'Elongated Muskrat',
+        content: 'What class is this?',
+        createdAt: new Date(),
+    },
+    {
+        uid: '1',
+        fakeName: 'Anonymous Moose',
+        content: 'content',
+        createdAt: new Date(),
+    },
+    {
+        uid: '1',
+        fakeName: 'Anonymous Moose',
+        content: 'more content',
+        createdAt: new Date(),
+    },
+    {
+        uid: '1',
+        fakeName: 'Anonymous Moose',
+        content: 'even more content',
+        createdAt: new Date(),
+    },
+    {
+        uid: '2',
+        fakeName: 'Angry Panda',
+        content: 'Can you go over problem 3?',
+        createdAt: new Date(),
+    },
+    {
+        uid: '2',
+        fakeName: 'Angry Panda',
+        content: 'Can you go over problem 4?',
+        createdAt: new Date(),
+    },
+    {
+        uid: '3',
+        fakeName: 'Elongated Muskrat',
+        content: 'What class is this?',
+        createdAt: new Date(),
+    },
 ];
